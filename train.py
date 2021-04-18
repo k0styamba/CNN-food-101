07fc44ebfc8f9af6a9782fd965ccb432829ff994
k0styamba <kostiasadovskiy@gmail.com>
2021-04-18 09:47:19 +0000

    imagenetTF

--- train.py
+++ train.py
@@ -56,7 +56,9 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  x = tf.keras.applications.EfficientNetB0(include_top=True, weights=None)(inputs)
+  model = tf.keras.applications.EfficientNetB0(include_top=False, input_tensor=inputs, weights='imagenet')
+  model.trainable = False
+  x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
   outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
   return tf.keras.Model(inputs=inputs, outputs=outputs)
 
