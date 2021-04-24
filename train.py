2263aa3531e5ff84e8bbdf45eec72251385638af
k0styamba <kostiasadovskiy@gmail.com>
2021-04-24 19:14:07 +0000

    CosineRes 0.0002

--- train.py
+++ train.py
@@ -88,7 +88,7 @@ def main():
     validation_data=validation_dataset,
     callbacks=[
       tf.keras.callbacks.TensorBoard(log_dir),
-      LearningRateScheduler(tf.keras.experimental.CosineDecayRestarts(0.002, 500, 2.0, 1.0, 0.0, None)),
+      LearningRateScheduler(tf.keras.experimental.CosineDecayRestarts(0.0002, 500, 2.0, 1.0, 0.0, None)),
     ]
   )
 
