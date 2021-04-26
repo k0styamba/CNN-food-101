b66f591a6a463c510fa116a60d9e9252bf85db7f
k0styamba <kostiasadovskiy@gmail.com>
2021-04-26 22:24:40 +0000

    CosineDecayRestarts(0.0005, 4000, 2.0, 1.0, 0.5, None)

--- train.py
+++ train.py
@@ -88,7 +88,7 @@ def main():
     validation_data=validation_dataset,
     callbacks=[
       tf.keras.callbacks.TensorBoard(log_dir),
-      LearningRateScheduler(tf.keras.experimental.CosineDecayRestarts(0.01, 1000, 1.0, 0.75, 0.001, None)),
+      LearningRateScheduler(tf.keras.experimental.CosineDecayRestarts(0.0005, 4000, 2.0, 1.0, 0.5, None)),
     ]
   )
 
